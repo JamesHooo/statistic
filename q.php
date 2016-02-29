49cedb68dada13d4fb33c8de1b785380c6f9f8b7
hushiwei <hushiwei@corp.netease.com>
2016-02-29 16:29:52 +0800

    区分请求数据

--- q.php
+++ q.php
@@ -1,29 +1,61 @@
 <table border=1 width=300>
 <?php
+
 $con=mysql_connect("127.0.0.1:5689","counter","1234");
 if(!$con)
 {
 	die ('Could not connect' . mysql_error());
 }
-
 mysql_select_db("counter", $con);
-$sql="SELECT from_unixtime(timestamp,'%Y-%m-%d') as t,act,count(1),count(distinct ip) from record group by t,act";
-$res=mysql_query($sql);
-print "<tr>";
-print "<th>Date</th>";
-print "<th>Act</th>";
-print "<th>PV</th>";
-print "<th>UV</th>";
-print "</tr>";
-while($row=mysql_fetch_array($res))
+
+if(isset($_SERVER['QUERY_STRING']))
 {
-	print "<tr>";
-	print "<td>".$row[0]."</td>";
-	print "<td>".$row[1]."</td>";
-	print "<td>".$row[2]."</td>";
-	print "<td>".$row[3]."</td>";
-	print "</tr>";
+	if($_SERVER['QUERY_STRING'] == 'hourly')
+	{
+		$sql="SELECT from_unixtime(timestamp,'%m-%d $H') as t,act,count(1),count(distinct ip) from record group by t,act";
+		$res=mysql_query($sql);
+		print "<tr>";
+		print "<th>Date</th>";
+		print "<th>Act</th>";
+		print "<th>PV</th>";
+		print "<th>UV</th>";
+		print "</tr>";
+		while($row=mysql_fetch_array($res))
+		{
+			print "<tr>";
+			print "<td>".$row[0]."</td>";
+			print "<td>".$row[1]."</td>";
+			print "<td>".$row[2]."</td>";
+			print "<td>".$row[3]."</td>";
+			print "</tr>";
+		}			
+	}
+	else 
+	{
+		print "q.php?hour or q.php";		
+	}
 }
+else 
+{
+	$sql="SELECT from_unixtime(timestamp,'%Y-%m-%d') as t,act,count(1),count(distinct ip) from record group by t,act";
+	$res=mysql_query($sql);
+	print "<tr>";
+	print "<th>Date</th>";
+	print "<th>Act</th>";
+	print "<th>PV</th>";
+	print "<th>UV</th>";
+	print "</tr>";
+	while($row=mysql_fetch_array($res))
+	{
+		print "<tr>";
+		print "<td>".$row[0]."</td>";
+		print "<td>".$row[1]."</td>";
+		print "<td>".$row[2]."</td>";
+		print "<td>".$row[3]."</td>";
+		print "</tr>";
+	}
+}
+
 mysql_close($con);
 ?>
 </table>
